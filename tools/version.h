3a65e1ae3ef8e8840ac7c29157a1f13c90024054
Steven Toth <stoth@kernellabs.com>
2020-04-14 14:06:28 -0400

    update copyright dates for certain files.

--- tools/version.h
+++ tools/version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) LiveTimeNet, Inc. 2017. All Rights Reserved.
+ * Copyright (c) LiveTimeNet, Inc. 2017-2020. All Rights Reserved.
  *
  * Address: LTN Global Communications, Inc.
  *          Historic Savage Mill
@@ -24,4 +24,4 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#define COPYRIGHT "Copyright (c) LiveTimeNet, Inc. 2017. All Rights Reserved."
+#define COPYRIGHT "Copyright (c) LiveTimeNet, Inc. 2017-2020. All Rights Reserved."
